6aaae7608f06efc8714926d30a88efde1bb00884
ErosKarm <48349453+ErosKarm@users.noreply.github.com>
2019-05-24 09:50:21 +0300

    yl1

--- praks4a/yl1.sh
+++ praks4a/yl1.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
+#küsime vastuseid
 echo -n "Palun sisestage oma nimi: "
 read nimi
 echo "Tere tulemast, $nimi!"
-
+#küsime veel vastuseid
 echo -n "Sisestage oma sünniaasta: "
 read su
+#teeme väikse arvutuse
 vanus=$(expr 2019 - $su)
 echo "Te olete $vanus aastat vana."
+#liialt lihtne skript et midagi rääkida
