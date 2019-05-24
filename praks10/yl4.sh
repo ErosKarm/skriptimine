56af5c86f66370299260869a0ed192635b9f5f1e
Eros Karm <eros.karm@khk.ee>
2019-05-24 09:45:35 +0300

    aaa

---
+++ praks10/yl4.sh
@@ -0,0 +1,15 @@
+echo -n -e "Sisestage palun siia aeg tunni väärtuses: "
+read aeg
+
+if [ $aeg -ge 6 -a $aeg -lt 12 ]; then
+echo "Tere hommikust $USER!"
+elif [ $aeg -ge 12 -a $aeg -lt 18 ]; then
+echo "Tere päevast $USER!"
+elif [ $aeg -ge 18 -a $aeg -lt 22 ]; then
+echo "Tere õhtust $USER!"
+elif [ $aeg -ge 22 -a $aeg -lt 24 ] || [ $aeg -ge 0 -a $aeg -lt 6 ];then
+echo "Head ööd $USER!"
+else
+echo "Palun sisestage aeg 24 tunni väärtuses."
+fi
+
