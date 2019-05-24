7e54d0b3bf94a026d8207dbe2a43909c17e93de4
ErosKarm <48349453+ErosKarm@users.noreply.github.com>
2019-05-24 09:52:48 +0300

    yl3

--- praks10/yl3.sh
+++ praks10/yl3.sh
@@ -1,6 +1,7 @@
+#/bin/bash#
 echo -n -e "Sisestage palun siia aeg tunni väärtuses: "
 read aeg
-
+#teeme vajalikud arvutused if-iga
 if [ $aeg -ge 6 -a $aeg -lt 12 ]; then
 echo "Tere hommikust"
 elif [ $aeg -ge 12 -a $aeg -lt 18 ]; then
