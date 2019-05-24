ffe3c1ea8600fd174036d4235279e12a63834ec0
ErosKarm <48349453+ErosKarm@users.noreply.github.com>
2019-05-24 09:51:43 +0300

    yl2

--- praks4a/yl2.sh
+++ praks4a/yl2.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
+#küsime vajalike vastuseid
 echo -n "Palun sisestage oma ringi raadius sentimeetrites: "
 read raadius
 echo -n "Teie ringi pindla on: "
+#teeme väikesed arvutused
 echo "$raadius * $raadius * 3.14" | bc 
 echo -n "Teie ringi ümbermõõt on: "
 echo "2 * 3.14 * $raadius" | bc
-
+#skripti lõpp
